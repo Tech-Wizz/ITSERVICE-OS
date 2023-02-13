c8a9585c5086f320639998d8d3b569c918953a75
Kruize Derek Christensen <christensen.kruize@gmail.com>
2023-02-13 11:31:28 -0700

    Made inputs

--- findOS.py
+++ findOS.py
@@ -5,20 +5,42 @@ from xlwt import Workbook
 wb = Workbook()
 sheet1 = wb.add_sheet('Sheet 1')
 
-n = 27
-while n < 28:
-    s = str(n)
-    os.system('cmd /c "color a & systeminfo /s 153.90.162.' + s + '|findstr /i \"host OS\" > e:\output' + s + '.txt"')
+ip = input('Enter first 3 sections of IP "###.###.###.\n')
+start = input('What number do you want to start on?\n')
+end = input('What number do you want to end with?\n')
+s = int(start)
+e = int(end)
+
+while s < (e + 1):
+
+
+    os.system('cmd /c "color a & systeminfo /s ' + ip + start + ' > e:\output' + start + '.txt"')
     #os.system('cmd /c "color a & systeminfo /s 153.90.162.' + s + '|findstr /i \"host OS\" > e:\output.txt"')
-    nameos = linecache.getline(r"E:\output" + s + ".txt", 2)
+
+    #converts the data from the txt file to varibles
+    
+    hostName = linecache.getline(r"E:\output" + start + ".txt", 2)
+    hostName = hostName.lstrip("Host Name:                 ")
+    systemModel = linecache.getline(r"E:\output" + start + ".txt", 14)
+    systemModel = systemModel.lstrip("System Model:              ")
+    versionOS = linecache.getline(r"E:\output" + start + ".txt", 4)
+    versionOS = versionOS.lstrip("OS Version:                   10.0")
+    nameos = linecache.getline(r"E:\output" + start + ".txt", 3)
     nameos = nameos.lstrip("OS Name:                   ")
-    versionOS = linecache.getline(r"E:\output" + s + ".txt", 3)
-    versionOS = versionOS.lstrip("OS Version:                   ")
-    sheet1.write(n-1,1, nameos)
-    sheet1.write(n-1,2, versionOS)
-    sheet1.write(n-1,0, "153.90.162." + s)
-    os.remove("E:\output" + s + ".txt")
-    n += 1
+    
+    ##puts the data collected 
+
+    row = s - 1
+    sheet1.write(row,0, hostName)
+    sheet1.write(row,1, "153.90.162." + start)
+    sheet1.write(row,2, systemModel)
+    sheet1.write(row,3, versionOS)
+    sheet1.write(row,4, nameos)
+    os.remove("E:\output" + start + ".txt")
+    s += 1
+    print("RAN 153.90.162." + start)
 
 
+#saves results to an excel document
 wb.save('computerOS.xls')
+print("Program Completed! Save in computerOS.xls!")
